46f98c58aeb5ab246cdfd1cc474b85dc1dcdf3f1
Sorp <danvuk11@gmail.com>
2021-03-09 18:41:20 +0200

    add track

---
+++ Lab4/Lab4/Track.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Lab4
+{
+    public class Track
+    {
+        private Int32 id;
+        private Int32 chunkSize;
+        private Int32 format;
+        
+        private Int32 subchunk1Id;
+        private Int32 subchunk1Size;
+        private Int16 audioFormat;
+        private Int16 numChannels;
+        private Int32 sampleRate;
+        private Int32 byteRate;
+        private Int16 blockAlign;
+        private Int16 bitsPerSample;
+
+        private Int32 subchunk2Id;
+        private Int32 subchunk2Size;
+        private byte[] data;
+
+    }
+}
