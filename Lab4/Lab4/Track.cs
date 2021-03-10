e901dd214d10e7f4f8b0812635bb8215f3c1c1d2
Nikita Kovalenko <nikkv.ottf@gmail.com>
2021-03-10 14:34:13 +0200

    renamed private fields to properties

--- Lab4/Lab4/Track.cs
+++ Lab4/Lab4/Track.cs
@@ -5,54 +5,54 @@ namespace Lab4
 {
     public class Track
     {
-        private Int32 id;
-        private Int32 chunkSize;
-        private Int32 format;
-        
-        private Int32 subchunk1Id;
-        private Int32 subchunk1Size;
-        private Int16 audioFormat;
-        private Int16 numChannels;
-        private Int32 sampleRate;
-        private Int32 byteRate;
-        private Int16 blockAlign;
-        private Int16 bitsPerSample;
+        public Int32 Id { get; private set; }
+        public Int32 ChunkSize { get; private set; }
+        public Int32 Format { get; private set; }
 
-        private Int32 subchunk2Id;
-        private Int32 subchunk2Size;
+        public Int32 SubChunk1Id { get; private set; }
+        public Int32 SubChunk1Size { get; set; }
+        public Int16 AudioFormat { get; private set; }
+        public Int16 NumChannels { get; private set; }
+        public Int32 SampleRate { get; private set; }
+        public Int32 ByteRate { get; private set; }
+        public Int16 BlockAlign { get; private set; }
+        public Int16 BitsPerSample { get; private set; }
+
+        public Int32 SubChunk2Id { get; private set; }
+        public Int32 SubChunk2Size { get; private set; }
+
         private byte[] data;
-        
+
         public void ScaleTrack(int scale)
         {
+            byte[] newData = new byte[data.Length * scale];
 
-            byte[] newData = new byte[data.Length*scale];
- 
-            int sampleSize=bitsPerSample/8;
-            
-            for (int i = 0; i < data.Length - sampleSize; i+=sampleSize)
+            int sampleSize = BitsPerSample / 8;
+
+            for (int i = 0; i < data.Length - sampleSize; i += sampleSize)
             {
                 for (int j = 0; j < scale; j++)
                 {
                     for (int k = 0; k < sampleSize; k++)
                     {
-                        newData[i * scale + j * sampleSize + k] = data[i+k];
+                        newData[i * scale + j * sampleSize + k] = data[i + k];
                     }
                 }
             }
-            
-            
+
             data = newData;
+
             OnDataChanged();
         }
 
         public override string ToString()
         {
-            byte[] arr = BitConverter.GetBytes(id);
-            byte[] arr1 = BitConverter.GetBytes(subchunk1Id);
-            byte[] arr2 = BitConverter.GetBytes(subchunk2Id);
+            byte[] arr = BitConverter.GetBytes(Id);
+            byte[] arr1 = BitConverter.GetBytes(SubChunk1Id);
+            byte[] arr2 = BitConverter.GetBytes(SubChunk2Id);
             return System.Text.Encoding.Default.GetString(arr) + System.Text.Encoding.Default.GetString(arr1) + System.Text.Encoding.Default.GetString(arr2);
         }
-        
+
         private void OnDataChanged()
         {
             /*
@@ -61,10 +61,10 @@ namespace Lab4
                 You can also think of this as the size
                 of the read of the subchunk following this
             */
-            
-            int NumSamples = data.Length * 8 / bitsPerSample;
-            subchunk2Size = NumSamples * numChannels * bitsPerSample / 8;
-            chunkSize = 4 + (8 + subchunk1Size) + (8 + subchunk2Size);
+
+            int NumSamples = data.Length * 8 / BitsPerSample;
+            SubChunk2Size = NumSamples * NumChannels * BitsPerSample / 8;
+            ChunkSize = 4 + (8 + SubChunk1Size) + (8 + SubChunk2Size);
         }
 
         public void Deserialize(FileStream stream)
@@ -72,24 +72,24 @@ namespace Lab4
             using (BinaryReader reader = new BinaryReader(stream))
             {
                 //Head
-                id = reader.ReadInt32();
-                chunkSize = reader.ReadInt32();
-                format = reader.ReadInt32();
-                
+                Id = reader.ReadInt32();
+                ChunkSize = reader.ReadInt32();
+                Format = reader.ReadInt32();
+
                 //Block 1
-                subchunk1Id = reader.ReadInt32();
-                subchunk1Size = reader.ReadInt32();
-                audioFormat = reader.ReadInt16();
-                numChannels = reader.ReadInt16();
-                sampleRate = reader.ReadInt32();
-                byteRate = reader.ReadInt32();
-                blockAlign = reader.ReadInt16();
-                bitsPerSample = reader.ReadInt16(); 
-                
+                SubChunk1Id = reader.ReadInt32();
+                SubChunk1Size = reader.ReadInt32();
+                AudioFormat = reader.ReadInt16();
+                NumChannels = reader.ReadInt16();
+                SampleRate = reader.ReadInt32();
+                ByteRate = reader.ReadInt32();
+                BlockAlign = reader.ReadInt16();
+                BitsPerSample = reader.ReadInt16();
+
                 //Block 2
-                subchunk2Id = reader.ReadInt32();
-                subchunk2Size = reader.ReadInt32();
-                data = reader.ReadBytes((int) reader.BaseStream.Length);
+                SubChunk2Id = reader.ReadInt32();
+                SubChunk2Size = reader.ReadInt32();
+                data = reader.ReadBytes((int)reader.BaseStream.Length);
             }
         }
 
@@ -98,29 +98,26 @@ namespace Lab4
             using (BinaryWriter writer = new BinaryWriter(stream))
             {
                 //HEAD
-                writer.Write(id);
-                writer.Write(chunkSize);
-                writer.Write(format);
-                
+                writer.Write(Id);
+                writer.Write(ChunkSize);
+                writer.Write(Format);
+
                 //Block 1
-                writer.Write(subchunk1Id);
-                writer.Write(subchunk1Size);
-                writer.Write(audioFormat);
-                writer.Write(numChannels);
-                writer.Write(sampleRate);
-                writer.Write(byteRate);
-                writer.Write(blockAlign);
-                writer.Write(bitsPerSample);
-                
+                writer.Write(SubChunk1Id);
+                writer.Write(SubChunk1Size);
+                writer.Write(AudioFormat);
+                writer.Write(NumChannels);
+                writer.Write(SampleRate);
+                writer.Write(ByteRate);
+                writer.Write(BlockAlign);
+                writer.Write(BitsPerSample);
+
                 //Block 2
-                writer.Write(subchunk2Id);
-                writer.Write(subchunk2Size);
+                writer.Write(SubChunk2Id);
+                writer.Write(SubChunk2Size);
                 writer.Write(data);
 
             }
         }
-
     }
-    
-    
 }
