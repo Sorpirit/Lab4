2e00861eb32957564df67298556731e1489d00e4
Nikita Kovalenko <nikkv.ottf@gmail.com>
2021-03-17 12:01:12 +0200

    refactored ScaleTrack

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -18,8 +18,8 @@ namespace Lab4
              * Main
              */
 
-            string path = @"Audio.wav";
-            string outPut = @"Audio2.wav";
+            string path = @"Test.wav";
+            string outPut = @"Test2.wav";
             Track tr = new Track();
             using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate))
             {
@@ -29,7 +29,7 @@ namespace Lab4
 
             Console.WriteLine(tr.ChunkSize);
 
-            tr.ScaleTrack(.2);
+            tr.ScaleTrack(.5);
 
             using (FileStream stream = new FileStream(outPut, FileMode.OpenOrCreate))
             {
@@ -40,32 +40,5 @@ namespace Lab4
 
             Console.ReadKey();
         }
-
-        //private static int[] ScaleArr(int scale,int sample,int[] inputArr)
-        //{
-        //    int[] resultArr = new int[inputArr.Length * scale];
-        //    for (int i = 0; i < inputArr.Length - sample; i+=sample)
-        //    {
-        //        for (int j = 0; j < scale; j++)
-        //        {
-        //            for (int k = 0; k < sample; k++)
-        //            {
-        //                resultArr[i * scale + j * sample + k] = inputArr[i+k];
-        //            }
-        //        }
-        //    }
-
-        //    return resultArr;
-        //}
-
-        /*    
-        public override string ToString()
-        {
-            byte[] arr = BitConverter.GetBytes(id);
-            byte[] arr1 = BitConverter.GetBytes(subchunk1Id);
-            byte[] arr2 = BitConverter.GetBytes(subchunk2Id);
-            return System.Text.Encoding.Default.GetString(arr) + System.Text.Encoding.Default.GetString(arr1) + System.Text.Encoding.Default.GetString(arr2);
-        }
-        */
     }
 }
